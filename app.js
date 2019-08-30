eef32af2460ba7a6be1ebe555067c9dba3f2df56
Weston Carpenter <wc.cc96@gmail.com>
2019-08-29 23:26:04 -0700

    game over functionality added with option to restart

--- app.js
+++ app.js
@@ -5,27 +5,27 @@ var wordChoice = ["miles davis", "bill evans", "john coltrane", "duke ellington"
     "oscar peterson", "ray charles", "frank sinatra", "stan getz",
     "louis armstrong", "buddy rich", "keith jarrett"];
 
-
-var triesLeft = 10;
-var correct = [];
-var incorrect = [];
-var gameOver = false;
-
-PickWord(wordChoice);
-FillBlank(correct);
-
 document.querySelector('#instructions').innerHTML = "Press any key to begin...";
 
-
-// User initializes letter
+// User initializes game
 document.onkeydown = function (event) {
+    
+    // Variables
+    var gameOver = false;
+    var triesLeft = 10;
+    var correct = [];
+    var incorrect = [];
 
+    // Setup
+    PickWord(wordChoice);
+    console.log(`The cat is ${word}`);
+    FillBlank(correct);
     PrintCorrect();
     document.querySelector('#instructions').innerHTML = "Guess a letter";
-    document.onkeydown = function (event) {
 
+    // User guesses a letter
+    document.onkeydown = function (event) {
         var letter = event.key;
-        //check if letter is a letter (not number or other key)
         SearchWord(word, letter);
         if (SearchWord(word, letter) === false) {
             AddIncorrect(incorrect, letter);
@@ -33,70 +33,79 @@ document.onkeydown = function (event) {
         PrintCorrect();
         PrintIncorrect();
         document.querySelector('#triesLeft').innerHTML = triesLeft + " tries left!";
+        CheckGameOver();
+        if(gameOver){
+            document.onkeydown = function (event) {
+            location.reload();
+            }
+        }
+    }
+    // Picks a random string from an array
+    function PickWord(choices) {
+        var randInt = Math.floor(Math.random() * 20);
+        word = choices[randInt];
+    }
+    // Fills characters of an array with underscores
+    function FillBlank(arr) {
+        for (let i = 0; i < word.length; i++) {
+            if (word[i] == " ") {
+                arr.push(" ");
+            } else
+                arr.push("_");
+        }
+    }
+    // Prints the letters that the user has guessed correctly in their respective spaces 
+    function PrintCorrect() {
+        var correctString = document.getElementById("correctGuess");
+        correctString.innerHTML = correct.join("").toUpperCase();
+    }
+    // Prints the letters that the user has guessed incorrectly
+    function PrintIncorrect() {
+        var incorrectString = document.getElementById("incorrectGuess");
+        incorrectString.innerHTML = incorrect.join(" ").toUpperCase();
+    }
+    // Searches a string for a character. 
+    // If found, the character is pushed into the matching index in the correct array.
+    function SearchWord(search, guess) {
+        var isLetter = false;
+        for (let i = 0; i < word.length; i++) {
+            if (guess === search.charAt(i)) {
+                correct[i] = guess;
+                isLetter = true;
+            }
+        }
+        return isLetter;
+    }
+    // If not already guessed, adds character to incorrect array and decrements triesLeft
+    function AddIncorrect(search, guess) {
+        if (search.indexOf(guess) > -1) {
+            console.log("Already guessed!");
+        } else {
+            search.push(guess);
+            triesLeft--;
+            document.querySelector('#instructions').innerHTML = "Incorrect Guesses:";
 
-        WonGameCheck();
-        LostGameCheck();
-    };
+        }
+    }
+    // Checks to see if the user has won or lost the game, and sends the result to UI
+    function CheckGameOver() {
+        if (!(correct.indexOf("_") > -1)) {
+            document.querySelector('#triesLeft').innerHTML = "";
+            document.querySelector('#incorrectGuess').innerHTML = "";
+            document.querySelector('#instructions').innerHTML = "You named that cat! Press any key to play again...";
+            gameOver = true;
+        }
+
+        if (triesLeft === 0) {
+            document.querySelector('#triesLeft').innerHTML = "";
+            document.querySelector('#incorrectGuess').innerHTML = "";
+            document.querySelector('#instructions').innerHTML = `The cat was ${word.toUpperCase()}! Press any key to play again...`;
+            gameOver = true;
+        }
+    }
 };
 
 
-
-function PickWord(choices) {
-    var randInt = Math.floor(Math.random() * 20);
-    word = choices[randInt];
-}
-function FillBlank(arr) {
-    for (let i = 0; i < word.length; i++) {
-        if (word[i] == " ") {
-            arr.push(" ");
-        } else
-            arr.push("_");
-    }
-}
-function PrintCorrect() {
-    var correctString = document.getElementById("correctGuess");
-    correctString.innerHTML = correct.join("").toUpperCase();
-}
-function PrintIncorrect() {
-    var incorrectString = document.getElementById("incorrectGuess");
-    incorrectString.innerHTML = incorrect.join(" ").toUpperCase();
-}
-function SearchWord(search, guess) {
-    var isLetter = false;
-    for (let i = 0; i < word.length; i++) {
-        if (guess === search.charAt(i)) {
-            correct[i] = guess;
-            isLetter = true;
-        }
-    }
-    return isLetter;
-}
-function AddIncorrect(search, guess) {
-    if (search.indexOf(guess) > -1) {
-        console.log("Already guessed!");
-    } else {
-        search.push(guess);
-        triesLeft--;
-        document.querySelector('#instructions').innerHTML = "Incorrect Guesses:";
-
-    }
-}
-function WonGameCheck() {
-    if (!(correct.indexOf("_") > -1)) {
-        document.querySelector('#triesLeft').innerHTML = "";
-        document.querySelector('#incorrectGuess').innerHTML = "";
-        document.querySelector('#instructions').innerHTML = "You named that cat! Press any key to play again...";
-        gameOver = true;
-    }
-}
-function LostGameCheck() {
-    if (triesLeft === 0) {
-        document.querySelector('#triesLeft').innerHTML = "";
-        document.querySelector('#incorrectGuess').innerHTML = "";
-        document.querySelector('#instructions').innerHTML = `The cat was ${word.toUpperCase()}! Press any key to play again...`;
-        gameOver = true;
-    }
-}
 
 
 
